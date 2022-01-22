1177db9442aed51c1355362c132b0c43ff927919
gcy2312 <78692599+gcy2312@users.noreply.github.com>
2022-01-21 19:18:25 -0500

    is showMore - map grades

--- src/components/Student/Student.jsx
+++ src/components/Student/Student.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './Student.css';
-import { TagsInput } from '../TagsInput/TagsInput';
+import TagsInput from '../TagsInput/TagsInput';
 import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
@@ -8,70 +8,94 @@ import AddRoundedIcon from '@mui/icons-material/AddRounded';
 import RemoveRoundedIcon from '@mui/icons-material/RemoveRounded';
 
 
-export function Student({ studentData, students, setStudentTags, setDisplayStudents }) {
-  let [isExpanded, setExpanded] = useState(false);
+export default class Student extends Component {
 
-  return (
-    <div className="student-card">
-      <Row>
-        <Card id="card">
-          <Col md={2} sm={2} xs={{ order: 'first' }}>
-            <img id="studentImg" src={studentData.pic} alt="student avatar" />
-          </Col> *
-          <Col md={10} sm={10} xs={10}>
-            <Card.Body>
+  state = {
+    showMore: false
+  }
+  showMore() {
+    this.setState({ showMore: !this.state.showMore });
+  }
+  expandGrades(studentData) {
+    let gradesArr = studentData.grades;
+    if (this.state.showMore === true) {
+      return (
+        <div>
+          {
+            gradesArr.map((grade, index) => (
+              <Card.Text key={index}>Test {index + 1}: <span className="addWidth" />{grade}%</Card.Text>
+            ))
+          }
+        </div>
+      );
+    } else {
+      return ""
+    }
+  }
 
-              <div id="cardHeader">
+  render() {
+    const { studentData, students } = this.props;
 
-                <Card.Title id="names">{studentData.firstName} {studentData.lastName}
-                </Card.Title>
-                <div
-                  id="detailsToggle"
-                  onClick={() => setExpanded(!isExpanded)}
-                >
-                  {isExpanded ? <RemoveRoundedIcon fontSize="large" color="disabled" /> : <AddRoundedIcon
-                    fontSize="large" color="disabled" />}
+    const selectedTags = tags => console.log(tags);
+
+    return (
+
+      <div className="student-card">
+        <Row>
+
+          <Card id="card">
+            <Col md={2} sm={2} xs={{ order: 'first' }}>
+              <img id="studentImg" src={studentData.pic} alt="student avatar" />
+            </Col> *
+            <Col md={10} sm={10} xs={10}>
+              <Card.Body>
+
+                <div id="cardHeader">
+
+                  <Card.Title id="names">{studentData.firstName} {studentData.lastName}
+                  </Card.Title>
+                  <div
+                    id="detailsToggle"
+                    onClick={() => { this.showMore() }}
+                  >
+                    {this.state.showMore ? <RemoveRoundedIcon fontSize="large" color="disabled" /> : <AddRoundedIcon
+                      fontSize="large" color="disabled" />}
+                  </div>
                 </div>
-              </div>
-              <div id="cardMainBody">
+                <div id="cardMainBody">
 
-                <Card.Text>Email: {studentData.email}</Card.Text>
-                <Card.Text>Company: {studentData.company}</Card.Text>
-                <Card.Text>Skill: {studentData.skill}</Card.Text>
-                <Card.Text>Average: {studentData.average}%</Card.Text>
-              </div>
-
-              <TagsInput
-                studentData={studentData}
-                setStudentTags={setStudentTags}
-                setDisplayStudents={setDisplayStudents}
-              />
-
-              {isExpanded && (
-                <div className="expandedCard">
-                  <Card.Text>Test 1: <span className="addWidth" />{studentData.grades[0]} %</Card.Text>
-                  <Card.Text>Test 2: <span className="addWidth" />{studentData.grades[1]} %</Card.Text>
-                  <Card.Text>Test 3: <span className="addWidth" />{studentData.grades[2]} %</Card.Text>
-                  <Card.Text>Test 4: <span className="addWidth" />{studentData.grades[3]} %</Card.Text>
-                  <Card.Text>Test 5: <span className="addWidth" />{studentData.grades[4]} %</Card.Text>
-                  <Card.Text>Test 6: <span className="addWidth" />{studentData.grades[5]} %</Card.Text>
-                  <Card.Text>Test 7: <span className="addWidth" />{studentData.grades[6]} %</Card.Text>
-                  <Card.Text>Test 8: <span className="addWidth" />{studentData.grades[7]} %</Card.Text>
+                  <Card.Text>Email: {studentData.email}</Card.Text>
+                  <Card.Text>Company: {studentData.company}</Card.Text>
+                  <Card.Text>Skill: {studentData.skill}</Card.Text>
+                  <Card.Text>Average: {studentData.average}%</Card.Text>
                 </div>
-              )}
+
+                <TagsInput
+                  studentData={studentData}
+                  selectedTags={selectedTags}
+                  students={students}
+                />
+
+                {this.state.showMore && (
+                  <div className="expandedCard">{this.expandGrades(studentData)}
+
+                  </div>
+                )}
 
 
-            </Card.Body>
+              </Card.Body>
 
-          </Col>
-
-
-        </Card>
-      </Row>
-      <div className="event-card__buttons">
+            </Col>
 
 
+          </Card>
+        </Row>
+        <div className="event-card__buttons">
+
+
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
+
 }
